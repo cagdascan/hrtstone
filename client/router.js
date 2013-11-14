4727408a24ec1fcaafcf34dec7c22680c1b2dbd2
momikus <yavuzkirez@gmail.com>
2013-11-14 09:40:14 +0200

    builddeck seo router undo

--- client/router.js
+++ client/router.js
@@ -1,40 +1,8 @@
 Meteor.Router.add({
 	'/'								: 'decks',
 	'/deck/miracle-rouge'			: 'singledeck',
-	'/build-deck/druid'				: function(){
+	'/build-deck'					: function(){
 										Session.set('class', 'Druid');
-										return 'builddeck';
- 									},
-	'/build-deck/hunter'				: function(){
-										Session.set('class', 'Hunter');
-										return 'builddeck';
- 									},
-	'/build-deck/mage'				: function(){
-										Session.set('class', 'Mage');
-										return 'builddeck';
- 									},
-	'/build-deck/paladin'				: function(){
-										Session.set('class', 'Paladin');
-										return 'builddeck';
- 									},
-	'/build-deck/priest'				: function(){
-										Session.set('class', 'Priest');
-										return 'builddeck';
- 									},
-	'/build-deck/rouge'				: function(){
-										Session.set('class', 'Rouge');
-										return 'builddeck';
- 									},
-	'/build-deck/shaman'				: function(){
-										Session.set('class', 'Shaman');
-										return 'builddeck';
- 									},
-	'/build-deck/warlock'				: function(){
-										Session.set('class', 'Warlock');
-										return 'builddeck';
- 									},
-	'/build-deck/warrior'				: function(){
-										Session.set('class', 'Warrior');
 										return 'builddeck';
  									}
 });
