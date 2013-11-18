997b62422701f6e69533399403d6000a27aab198
ccan <cagdascan@gmail.com>
2013-11-18 12:44:02 +0200

    do not allow short deckname on save

--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ Session.set('page_count', 1);
 Session.set('deck_sort', 'top');
 Session.set('selected_class', 'All Classes');
 Session.set('deck_limit', 10);
+Session.set('save_error', '');
 
 Deck = new Meteor.Collection(null); //local collection for deck building
 
