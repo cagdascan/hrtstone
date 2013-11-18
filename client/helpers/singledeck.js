31df03e65598551b65294c93627282f8bc0c4ad6
ccan <cagdascan@gmail.com>
2013-11-18 20:37:29 +0200

    single deck mana curve done

--- client/helpers/singledeck.js
+++ client/helpers/singledeck.js
@@ -10,6 +10,9 @@ Template.singledeck.helpers({
 		var email = Meteor.users.findOne().emails[0].address;
 		return Gravatar.imageUrl(email);
 	},
+	current_username: function () {
+		return Meteor.users.findOne().username
+	},
 	comments: function () {
 		return Comments.find({}, {sort:{timestamp: -1}}).fetch();
 	},
@@ -30,6 +33,106 @@ Template.singledeck.helpers({
 			return true;
 		else
 			return false;
+	},
+	mana_count: function (mana) {
+		var mana = mana;
+		var total = 0;
+		var decklist = Decks.find().fetch()[0].decklist;
+		if (mana != '7'){
+			decklist.forEach(function(i){
+				if (i.cost == mana)
+					total = total  + i.count;
+			});
+		}
+		else
+			decklist.forEach(function(i){
+				if (i.cost == '7'  || i.cost == '8'  ||
+						i.cost == '9'  || i.cost == '10' ||
+						i.cost == '12' || i.cost == '20'
+					 )
+					total = total  + i.count;
+			});
+
+		return total;
+	},
+	height: function (mana) {
+		var mana = mana;
+		var heigth = 0;
+		var total_0 = 0;
+		var total_1 = 0;
+		var total_2 = 0;
+		var total_3 = 0;
+		var total_4 = 0;
+		var total_5 = 0;
+		var total_6 = 0;
+		var total_7 = 0;
+		var max = 0;
+		var decklist = Decks.find().fetch()[0].decklist;
+
+		decklist.forEach(function(i){
+			if (i.cost == "0")
+				total_0 = total_0 + i.count;
+			else if (i.cost == "1")
+				total_1 = total_1 + i.count;
+			else if (i.cost == "2")
+				total_2 = total_2 + i.count;
+			else if (i.cost == "3")
+				total_3 = total_3 + i.count;
+			else if (i.cost == "4")
+				total_4 = total_4 + i.count;
+			else if (i.cost == "5")
+				total_5 = total_5 + i.count;
+			else if (i.cost == "6")
+				total_6 = total_6 + i.count;
+			else if (i.cost == "7"  || i.cost == "8"  || i.cost == "9" ||
+							 i.cost == "10" || i.cost == "12" || i.cost == "20")
+				total_7 = total_7 + i.count;
+		});
+
+		var total_array = [total_0, total_1, total_2, total_3, total_4, total_5, total_6, total_7];
+
+		max = _.max(total_array);
+
+		if (mana == "0")
+			if (total_0 == max)
+				return 80;
+			else 
+				return Math.ceil(total_0 / max * 100);
+		else if (mana == "1")
+			if (total_1 == max)
+				return 80;
+			else 
+				return Math.ceil(total_1 / max * 100);
+		else if (mana == "2")
+			if (total_2 == max)
+				return 80;
+			else 
+				return Math.ceil(total_2 / max * 100);
+		else if (mana == "3")
+			if (total_3 == max)
+				return 80;
+			else 
+				return Math.ceil(total_3 / max * 100);
+		else if (mana == "4")
+			if (total_4 == max)
+				return 80;
+			else 
+				return Math.ceil(total_4 / max * 100);
+		else if (mana == "5")
+			if (total_5 == max)
+				return 80;
+			else 
+				return Math.ceil(total_5 / max * 100);
+		else if (mana == "6")
+			if (total_6 == max)
+				return 80;
+			else 
+				return Math.ceil(total_6 / max * 100);
+		else if (mana == "7")
+			if (total_7 == max)
+				return 80;
+			else 
+				return Math.ceil(total_7 / max * 100);
 	}
 });
 
