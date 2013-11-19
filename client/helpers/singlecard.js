bbc44f2ca9aed7ac2096fcfdb26b0de5f38e678f
ccan <cagdascan@gmail.com>
2013-11-19 12:52:00 +0200

    markdown failed lol

--- client/helpers/singlecard.js
+++ client/helpers/singlecard.js
@@ -1,5 +1,11 @@
 Template.singlecard.helpers({
 	card: function () {
 		return Cards.find();
+	},
+	card_class: function () {
+		if (this.class == '')
+			return 'Neutral'
+		else
+			return this.class;
 	}
 });
