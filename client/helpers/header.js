f25dd09944d1b94a0b718a30d688c00dcce9eca7
ccan <cagdascan@gmail.com>
2013-11-20 14:49:43 +0200

    user links

--- client/helpers/header.js
+++ client/helpers/header.js
@@ -3,5 +3,6 @@ Template.header.rendered = function () {
 		$('nav li#decks').addClass('active');
 	}
 	var aim = $('#login-buttons-open-change-password');
-	$(aim).parent().html('<a href="YAZARSIN HACI"class="btn btn-tome btn-block">Go to my decks</a><button class="btn btn-default btn-block" id="login-buttons-open-change-password">Change Password</button><button class="btn btn-block btn-primary" id="login-buttons-logout">Sign out</button>');
+	var userid = Meteor.userId();
+	$(aim).parent().html('<a href="/user/' + userid + '" class="btn btn-tome btn-block">Go to my decks</a><button class="btn btn-default btn-block" id="login-buttons-open-change-password">Change Password</button><button class="btn btn-block btn-primary" id="login-buttons-logout">Sign out</button>');
 };
